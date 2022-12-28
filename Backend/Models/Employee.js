38d86d6fc8d84c7d05a538844c490cba2582e10e
Nehagaddam2909 <nehagaddam2909@gmail.com>
2022-12-28 22:51:22 +0530

    Added validation rule for the schema

--- Backend/Models/Employee.js
+++ Backend/Models/Employee.js
@@ -1,19 +1,22 @@
 //Schema for the Employee
 const mongoose=require("mongoose")
 const Schema=mongoose.Schema;
+const {isEmail}=require("validator")
 
 const EmployeeSchema=new Schema({
     email:{
         title:String,
-        required:true,
+        required:[true,"Email can't be empty"],
+        validate:[isEmail,"Enter valid email"]
     },
     username:{
         title:String,
-        required:true,
+        required:[true,"USername can't be empty"],
     },
     password:{
         title:String,
         required:true,
+        minLength:[6,"Min length of the password is 6 characters"]
     },
     skills:[{
         skill_name:{
