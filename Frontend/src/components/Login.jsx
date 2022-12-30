b57d45b59cffda04812b70945c3b9474b084a860
Brij2 <brij4268@gmail.com>
2022-12-30 12:40:19 +0530

    LoginUi

---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../App.css'
+
+
+function Login() {
+  return (
+    <div className="Login bg-gray-200 h-screen flex items-center justify-center">
+      <center>
+      <div className=''>
+        <div className="container bg-white w-80 ">
+          <h1 className='text-2xl p-2'>
+            Login
+          </h1>
+          <h4 className='text-left mx-10'>Email address or username</h4>
+          <input type="text" className='border-2 border-gray-500 m-3 w-3/4'/>
+          <h4 className='text-left mx-10'>Password</h4>
+          <input type="password"  className='border-2 border-gray-500 m-3 w-3/4'/>
+          <a className='text-[#4DA6FF]' href="#"><h4 className='text-left mx-10'>Forgot password</h4></a>
+
+          <button className='border-2 border-gray-400 m-3 w-3/4 bg-[#4DA6FF]' type="submit">Login</button>
+          <br />
+          <a className='text-[#4DA6FF] my-2' href="#">Don't have an account?</a>
+        </div>
+      </div>
+      </center>
+    </div>
+  )
+}
+
+export default Login
