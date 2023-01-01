21296e61cda4916bab8d9255d89708ca17f883ab
Nehagaddam2909 <nehagaddam2909@gmail.com>
2023-01-02 02:01:15 +0530

    Added skills

--- FrontEnd1/src/pages/dashboard/notifications.jsx
+++ FrontEnd1/src/pages/dashboard/notifications.jsx
@@ -1,58 +1,196 @@
 import React from "react";
+import { useEffect,useState } from "react";
+
 import {
   Typography,
   Alert,
   Card,
   CardHeader,
   CardBody,
+  Button,
 } from "@material-tailwind/react";
 import { InformationCircleIcon } from "@heroicons/react/24/outline";
 
 export function Notifications() {
-  const [showAlerts, setShowAlerts] = React.useState({
-    blue: true,
-    green: true,
-    orange: true,
-    red: true,
-  });
-  const [showAlertsWithIcon, setShowAlertsWithIcon] = React.useState({
-    blue: true,
-    green: true,
-    orange: true,
-    red: true,
-  });
-  const alerts = ["blue", "green", "orange", "red"];
+  
+ const data={
+  "PM":[
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+  ],
+  "HR":[
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+  ],
+  "Management":[
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+  ],
+  "Technical":[
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+    {
+      skill_name:"abc",
+      
+    },
+  ]
+ }
+const d=Object.keys(data)
+  const onS=(e)=>{
+    const data=[...formFields]
+    data.push({skill_name:'',level:'',YOE:''})
+    console.log(data)
+    setformFields(data)
+  }
+  const onR=(e)=>{
+    const data=[...formFields]
+   data.pop();
+    setformFields(data)
+  }
+
+  const [t,changeToggle]=useState(true)
+ 
 
   return (
-    <div className="mx-auto my-20 flex max-w-screen-lg flex-col gap-8">
-      <Card>
-        <CardHeader
-          color="transparent"
-          floated={false}
-          shadow={false}
-          className="m-0 p-4"
-        >
-          <Typography variant="h5" color="blue-gray">
-            Alerts
-          </Typography>
-        </CardHeader>
-        <CardBody className="flex flex-col gap-4 p-4">
-          {alerts.map((color) => (
-            <Alert
-              key={color}
-              show={showAlerts[color]}
-              color={color}
-              dismissible={{
-                onClose: () =>
-                  setShowAlerts((current) => ({ ...current, [color]: false })),
-              }}
-            >
-              A simple {color} alert with an <a href="#">example link</a>. Give
-              it a click if you like.
-            </Alert>
-          ))}
-        </CardBody>
-      </Card>
+    <div className="mx-auto my-8 md:my-15 flex min-w-sm flex-col gap-8">
+     
+       
+            
+       <Typography variant="h3" color="purple">Skills</Typography>
+       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        
+        <div className="px-4 md:px-8 justify-center bg-white shadow-md rounded-xl h-28 m-4">
+          <input type="radio" name="domain" className=""></input>
+          <label className="text-xl">Technical</label>
+        </div>
+        <div className="px-4 md:px-8 justify-center bg-white shadow-md rounded-xl h-28 m-4">
+          <input type="radio" name="domain" className=""></input>
+          <label className="text-xl">Technical</label>
+        </div>
+        <div className="px-4 md:px-8 justify-center bg-white shadow-md rounded-xl h-28 m-4">
+          <input type="radio" name="domain" className=""></input>
+          <label className="text-xl">Technical</label>
+        </div>
+        <div className="px-4 md:px-8 justify-center bg-white shadow-md rounded-xl h-28 m-4">
+          <input type="radio" name="domain" className=""></input>
+          <label className="text-xl">Technical</label>
+        </div>
+       </div>
+       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+       <div className="flex-col px-1 md:px-3 mx-4 justify-center bg-white shadow-md rounded-xl h-24 m-4">
+          <input type="checkbox" name="domain1" className="" value={t} onChange={e=>changeToggle(!t)}></input>
+          <label httmfor="domain1" className="text-xl">Technical</label>
+          <div className={`grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 ${t?"hidden":""}`}>
+          <select className=" bg-white mb-2 lg:mb-0">
+            <option>Beginner</option>
+            <option>Intermediate</option>
+            <option>Expert</option>
+          </select>
+          <input type="number" className="w-full border border-black focus:outline-purple" name="exp"></input>
+          </div>
+        </div>
+        <div className="px-1 md:px-2 mx-4 justify-center bg-white shadow-md rounded-xl h-24 m-4">
+          <input type="checkbox" name="domain" className=""></input>
+          <label className="text-xl">Technical</label>
+        </div>
+        <div className="px-1 md:px-2 mx-4 justify-center bg-white shadow-md rounded-xl h-24 m-4">
+          <input type="checkbox" name="domain" className=""></input>
+          <label className="text-xl">Technical</label>
+        </div>
+        <div className="px-1 md:px-2 mx-4 justify-center bg-white shadow-md rounded-xl h-24 m-4">
+          <input type="checkbox" name="domain" className=""></input>
+          <label className="text-xl">Technical</label>
+        </div>
+        <div className="px-1 md:px-2 mx-4 justify-center bg-white shadow-md rounded-xl h-24 m-4">
+          <input type="checkbox" name="domain" className=""></input>
+          <label className="text-xl">Technical</label>
+        </div>
+        <div className="px-1 md:px-2 mx-4 justify-center bg-white shadow-md rounded-xl h-24 m-4">
+          <input type="checkbox" name="domain" className=""></input>
+          <label className="text-xl">Technical</label>
+        </div>
+        <div className="px-1 md:px-2 mx-4 justify-center bg-white shadow-md rounded-xl h-24 m-4">
+          <input type="checkbox" name="domain" className=""></input>
+          <label className="text-xl">Technical</label>
+        </div>
+        <div className="px-1 md:px-2 mx-4 justify-center bg-white shadow-md rounded-xl h-24 m-4">
+          <input type="checkbox" name="domain" className=""></input>
+          <label className="text-xl">Technical</label>
+        </div>
+        <div className="px-1 md:px-2 mx-4 justify-center bg-white shadow-md rounded-xl h-24 m-4">
+          <input type="checkbox" name="domain" className=""></input>
+          <label className="text-xl">Technical</label>
+        </div>
+       </div>
+       
       <Card>
         <CardHeader
           color="transparent"
@@ -65,26 +203,7 @@ export function Notifications() {
           </Typography>
         </CardHeader>
         <CardBody className="flex flex-col gap-4 p-4">
-          {alerts.map((color) => (
-            <Alert
-              key={color}
-              show={showAlertsWithIcon[color]}
-              color={color}
-              icon={
-                <InformationCircleIcon strokeWidth={2} className="h-6 w-6" />
-              }
-              dismissible={{
-                onClose: () =>
-                  setShowAlertsWithIcon((current) => ({
-                    ...current,
-                    [color]: false,
-                  })),
-              }}
-            >
-              A simple {color} alert with an <a href="#">example link</a>. Give
-              it a click if you like.
-            </Alert>
-          ))}
+          
         </CardBody>
       </Card>
     </div>
