f750594b9a970d784426482db4addceeff1c43d4
vishal <vislearn1983@gmail.com>
2023-01-03 13:55:51 +0530

    working on login-cookies handler

--- Backend/Controllers/index.js
+++ Backend/Controllers/index.js
@@ -1,40 +1,57 @@
-const Employee=require("../Models/Employee")
-const dotenv=require("dotenv").config()
-const jwt=require("jsonwebtoken");
+const Employee = require("../Models/Employee");
+const dotenv = require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { restart } = require("nodemon");
-
+const cookies = require("cookies");
 //require Authentication
-const requireAuth=(req,res,next)=>{
-    const token=req.cookies;
-    console.log(token)
-    if(token)
-    {
-        token=token.jwt
-        jwt.verify(token,process.env.Secrete_key,(err,decodedToken)=>{
-            if(err)
-            {
-                // console.log(err.message);
-                // res.redirect("http://localhost:5173/auth/sign-in");
-                res.redirect("/")
-            }
-            else{
-                // console.log(decodedToken);
-                req.body.id=decodedToken._id;
-                next();
-            }
-        })
-    }
-    else{
-        res.redirect("");
+const requireAuth = (req, res, next) => {
+  const token = req.cookie;
+  //   const token = req.cookie.jwt;
+  console.log(token);
+  if (token) {
+    // token = token.jwt;
+    jwt.verify(token, process.env.Secrete_key, (err, decodedToken) => {
+      if (err) {
+        // console.log(err.message);
+        res.redirect("http://localhost:5173/auth/sign-in");
+        // res.redirect("/");
+      } else {
+        // console.log(decodedToken);
+        req.body.id = decodedToken._id;
+        next();
+      }
+    });
+  } else {
+    res.redirect("http://localhost:5173/auth/sign-in");
 
-        //res.redirect("")
-    }
-}
+    //res.redirect("")
+  }
+};
 
-module.exports={requireAuth}
+// const requireAuth = (req, res, next) => {
+//   token_from_server = req.cookie;
+
+//   if (token_from_server) {
+//     jwt.verify(
+//       token_from_server,
+//       process.env.Secrete_key,
+//       (err, decodedToken) => {
+//         if (err) {
+//           console.log(err.message);
+//           res.redirect("http://localhost:5173/auth/sign-in");
+//         } else {
+//           console.log(decodedToken);
+//           next();
+//         }
+//       }
+//     );
+//   } else {
+//     console.log("token is not verify/created");
+//     res.redirect("http://localhost:5173/auth/sign-in");
+//   }
+// };
+
+module.exports = { requireAuth };
 //controller for the post login
-
-
-
 
 //
--- Backend/Models/Manager.js
+++ Backend/Models/Manager.js
@@ -1,16 +1,43 @@
 //Schema for the Manager
-const mongoose=require("mongoose")
-const Schema=mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const ManagerSchema= new Schema({
-    username:{
-        type:Schema.Types.username,
-        ref:"Employee"
-    },
-    password:{
-        type:Schema.Types.password,
-        ref:"Employee"
+const ManagerSchema = new Schema({
+  username: {
+    type: Schema.Types.username,
+    ref: "Employee",
+  },
+  password: {
+    type: Schema.Types.password,
+    ref: "Employee",
+    minLength: [8, "Min length of the password is 8 characters"],
+  },
+});
+
+module.exports = mongoose.model("Manager", ManagerSchema);
+
+//Encrypt the password
+ManagerSchema.pre("save", async function (next) {
+  // console.log(this)
+  const salt = await bcrypt.genSalt();
+  this.Password = await bcrypt.hash(this.Password, salt);
+  // console.log(this.Password)
+  next();
+});
+
+//Checking the login
+ManagerSchema.statics.login = async function (username, password) {
+  const auth = await this.findOne({ username: username });
+  if (auth) {
+    const dd = await bcrypt.compare(password, auth.Password);
+
+    if (dd) {
+      // console.log(auth)
+      return auth;
     }
-})
+    throw Error("Incorrect password ");
+  }
+  throw Error("Incorrecet email");
+};
 
-module.exports=mongoose.model("Manager",ManagerSchema);
+module.exports = mongoose.model("Employee", EmployeeSchema);
