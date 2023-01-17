0aab517aa78842e412d43362ed8c4410cf71d7f4
Nehagaddam2909 <nehagaddam2909@gmail.com>
2023-01-17 21:33:32 +0530

    Commit

--- Backend/index.js
+++ Backend/index.js
@@ -34,8 +34,8 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("Access-Control-Allow-Methods", "*")
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Methods", "*")
+  // res.setHeader("Access-Control-Allow-Origin", "*");
 
 
   next();
