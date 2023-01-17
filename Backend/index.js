74b35f784efbb5444d2fc9a457ce92d710492e9d
Nehagaddam2909 <nehagaddam2909@gmail.com>
2023-01-17 22:23:22 +0530

    Updated cors

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ const MyRoutes = require("./Routes");
 
 //token authentication
 const corsOptions = {
-  origin: ["http://localhost:5173","https://skillhub.onrender.com/","https://skillhub-akatsaki.netlify.app","https://skillhub.vercel.app"],
+  origin: true,
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
